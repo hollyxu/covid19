b7c0433e612dcff01254d69fd27ccf9aa6831ba5
Holly <holly@Helens-iMac.local>
2020-03-25 19:50:05 -0400

    TSX, semantic-ui, folder directory

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+import { IntroPage } from "./pages/IntroPage";
+
+function App() {
+  return (
+    <div className="App">
+      <IntroPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/IntroPage.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+import { Button } from "semantic-ui-react";
+
+export function IntroPage() {
+  return <Button>hi</Button>;
+}
